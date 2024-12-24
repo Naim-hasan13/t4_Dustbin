a1938815ed73130b297f01db03b353462741b767
sid <jahidtanbir69@gmail.com>
2024-12-24 17:24:44 +0530

    sid

---
+++ app/src/main/jni/keys.c
@@ -0,0 +1,23 @@
+#include <jni.h>
+
+
+
+JNIEXPORT jstring JNICALL
+Java_com_dustbin_hindcash_SplashActivity_Hatbc(JNIEnv *env, jobject thiz) {
+    return (*env)->NewStringUTF(env, "mgfdasgjourebbkjhrefhkvfsdgjjjpv");
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_dustbin_hindcash_MainActivity_Hatbc(JNIEnv *env, jobject thiz) {
+    return (*env)->NewStringUTF(env, "mgfdasgjourebbkjhrefhkvfsdgjjjpv");
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_dustbin_hindcash_ExchangeActivity_Hatbc(JNIEnv *env, jobject thiz) {
+    return (*env)->NewStringUTF(env, "mgfdasgjourebbkjhrefhkvfsdgjjjpv");
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_dustbin_hindcash_RedeemActivity_Hatbc(JNIEnv *env, jobject thiz) {
+    return (*env)->NewStringUTF(env, "mgfdasgjourebbkjhrefhkvfsdgjjjpv");
+}
